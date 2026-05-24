62a7f82f754815e2757335ea553356d4e67e3e43
Aleksander Ivanov <alex@aiivanov.dev>
2026-05-24 20:02:02 +0200

    Use centralized not implemented error for logout

--- app/server/internal/auth/logout.go
+++ app/server/internal/auth/logout.go
@@ -3,9 +3,10 @@ package auth
 import (
 	"net/http"
 
+	"github.com/vow/app/server/internal/shared/apperror"
 	"github.com/vow/app/server/internal/shared/response"
 )
 
 func (h Handler) Logout(w http.ResponseWriter, r *http.Request) {
-	response.Error(w, http.StatusNotImplemented, "logout not implemented")
+	response.HandleError(w, apperror.NotImplemented("AUTH_LOGOUT_NOT_IMPLEMENTED", "logout is not implemented yet"))
 }
