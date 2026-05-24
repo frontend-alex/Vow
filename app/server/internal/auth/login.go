fc8594bfc0c1ad9f8c130ecba5b6d1b37672c096
Aleksander Ivanov <alex@aiivanov.dev>
2026-05-24 20:01:50 +0200

    Use global error handler in login endpoint

--- app/server/internal/auth/login.go
+++ app/server/internal/auth/login.go
@@ -1,7 +1,6 @@
 package auth
 
 import (
-	"errors"
 	"net/http"
 
 	"github.com/vow/app/server/internal/shared/request"
@@ -11,18 +10,13 @@ import (
 func (h Handler) Login(w http.ResponseWriter, r *http.Request) {
 	input, err := request.DecodeAndValidate[LoginRequest](w, r)
 	if err != nil {
-		handleRequestError(w, err)
+		response.HandleError(w, err)
 		return
 	}
 
 	result, err := h.service.Login(r.Context(), input)
 	if err != nil {
-		if errors.Is(err, ErrInvalidCredentials) {
-			response.Error(w, http.StatusUnauthorized, "invalid email or password")
-			return
-		}
-
-		response.Error(w, http.StatusBadRequest, err.Error())
+		response.HandleError(w, err)
 		return
 	}
 
